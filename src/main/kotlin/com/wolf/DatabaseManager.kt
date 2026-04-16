e7c24c3b0f2ffed845bba88b540f96767f0d8c84
piro <ogakura.5656@gmail.com>
2026-04-16 15:48:20 +0900

    中身いれた

--- src/main/kotlin/com/wolf/DatabaseManager.kt
+++ src/main/kotlin/com/wolf/DatabaseManager.kt
@@ -1,2 +1,27 @@
-package com.wolf 
+package com.wolf
+
+import com.zaxxer.hikari.HikariConfig
+import com.zaxxer.hikari.HikariDataSource
+
+object DatabaseManager {
+
+    private lateinit var dataSource: HikariDataSource
+
+    fun connect(host: String, port: Int, database: String, user: String, password: String) {
+        val config = HikariConfig()
+        config.jdbcUrl = "jdbc:mysql://$host:$port/$database"
+        config.username = user
+        config.password = password
+        config.maximumPoolSize = 5
+        dataSource = HikariDataSource(config)
+    }
+
+    fun disconnect() {
+        if (::dataSource.isInitialized) {
+            dataSource.close()
+        }
+    }
+
+    fun getConnection() = dataSource.getConnection()
+}
 
--- src/main/kotlin/com/wolf/WolfPlugin.kt
+++ src/main/kotlin/com/wolf/WolfPlugin.kt
@@ -1,2 +1,22 @@
-package com.wolf 
+package com.wolf
 
+import org.bukkit.plugin.java.JavaPlugin
+
+class WolfPlugin : JavaPlugin() {
+
+    override fun onEnable() {
+        DatabaseManager.connect(
+            host = "localhost",
+            port = 3306,
+            database = "wolfdb",
+            user = "root",
+            password = ""
+        )
+        logger.info("WolfPluginが起動しました")
+    }
+
+    override fun onDisable() {
+        DatabaseManager.disconnect()
+        logger.info("WolfPluginが停止しました")
+    }
+}
